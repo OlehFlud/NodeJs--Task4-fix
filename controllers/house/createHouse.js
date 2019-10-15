a83e7a8b04d57ebe66d7ad255d3cc0d236ad0a7a
Oleh <52893252+Oleh999@users.noreply.github.com>
2019-10-15 20:58:58 +0300

    Update createHouse.js

--- controllers/house/createHouse.js
+++ controllers/house/createHouse.js
@@ -1,34 +1,3 @@
-// // const {provider} = require('../../dataBase');
-// //
-// // module.exports = async (req,res) => {
-// //
-// //     const {city,area,price,street} = req.body;
-// //
-// //     const query = `INSERT INTO houses (city,area,price,street) VALUES(${city},${area},${price},${street})`;
-// //
-// //     await provider.promise().query(query);
-// //
-// //     res.render('createHouse');
-// //
-// // };
-// const {provider} = require('../../dataBase');
-//
-// module.exports = async (req,res) => {
-//
-//
-//     const {city,area,price,street} = req.body;
-//
-//     const query = `INSERT INTO houses (city,area,price,street) VALUES(?,?,?,?)`;
-//
-//     await provider.promise().query(query,[city,area,price,street]);
-//
-//     res.render('createHouse');
-//
-//
-// };
-//
-//
-//
 const dataBase = require('../../dataBase').getInstance();
 
 module.exports = async (req,res) => {
